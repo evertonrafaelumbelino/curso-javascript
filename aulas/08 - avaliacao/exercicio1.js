cd0e02d389f362ffc8186d870108bacfb3791ad8
Everton Rafael Umbelino dos Santos <169084381+evertonrafaelumbelino@users.noreply.github.com>
2024-11-28 17:34:21 -0300

    criei minha tentativa do exercicio 1

--- aulas/08 - avaliacao/exercicio1.js
+++ aulas/08 - avaliacao/exercicio1.js
@@ -1,5 +1,5 @@
 /*
-1) Faça um programa que receba a média de uma aluno.
+1) Faça um programa que receba a média de um aluno.
 Caso a média seja < 5 imprima "Reprovado"
 Caso a média seja >= 5 e < 7 imprima "Recuperação"
 Caso a média seja >= 7 imprima "Aprovado"
@@ -11,4 +11,14 @@ Exemplo:
         Recuperação
 */
 
+/* Minha tentativa:
+
 const {gets, print} = require('./funcoes-auxiliares-ex1')
+
+if (gets() < 5) {
+    print('Reprovado')
+} else if (gets() >= 5 && gets() < 7) {
+    print('Recuperação')
+} else {
+    print('Aprovado')
+} */
---
+++ aulas/08 - avaliacao/funcoes-auxiliares-ex1.js
@@ -0,0 +1,20 @@
+/* Minha tentativa:
+
+const notas = [4, 5]
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    soma = soma + notas[i]
+}
+
+function gets() {
+    const media = soma / notas.length
+    return media
+}
+
+function print(texto) {
+    console.log(texto)
+}
+
+module.exports = {gets, print} */
