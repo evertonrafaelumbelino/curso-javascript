fdf8449acf976d2ecfa49dcb42ceb7035311c0c7
Everton Rafael Umbelino dos Santos <169084381+evertonrafaelumbelino@users.noreply.github.com>
2024-11-29 19:36:53 -0300

    atualizei exercicio 2

--- aulas/08 - avaliacao/exercicio2.js
+++ aulas/08 - avaliacao/exercicio2.js
@@ -38,3 +38,25 @@ print(numeroPar)
 print(numeroImpar) */
 
 const {gets, print} = require('./funcoes-auxiliares-ex2')
+
+const n = gets()
+
+let maiorNumeroPar = null;
+let menorNumeroImpar = null;
+
+for (let i = 0; i < n; i++) {
+    const numero = gets()
+
+    if (numero % 2 === 0) {
+        if (maiorNumeroPar === null || numero > maiorNumeroPar) {
+        maiorNumeroPar = numero
+    }
+    } else {
+        if (menorNumeroImpar === null || numero < menorNumeroImpar) {
+            menorNumeroImpar = numero
+        }
+    }
+}
+
+print('Maior número par: ' + maiorNumeroPar)
+print('Menor número impar: ' + menorNumeroImpar)
