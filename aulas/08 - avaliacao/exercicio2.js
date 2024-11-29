5e2b9c838eec2f2852257c9ccab2e49e41c61374
Everton Rafael Umbelino dos Santos <169084381+evertonrafaelumbelino@users.noreply.github.com>
2024-11-29 15:27:30 -0300

    criei minha tentativa do exercicio 2

---
+++ aulas/08 - avaliacao/exercicio2.js
@@ -0,0 +1,38 @@
+/* 2) Faça um programa que receba N (quantidade de números) e seus respectivos valores.
+Imprima o maior número par e o menor número impar. 
+
+Exemplo:
+    Entrada:
+        5
+        3
+        4
+        1
+        10
+        8
+
+    Saída:
+        Maior número par: 10
+        Menor número impar: 1
+*/
+
+/* Minha tentativa:
+
+const {gets, print} = require('./funcoes-auxiliares-ex2')
+
+const n = gets()
+
+let numeroPar = 0;
+let numeroImpar = 0;
+
+for (let i = 0; i < n.length; i++) {
+    if (n[i] > numeroPar) {
+        numeroPar = n[i]
+    }
+
+    if (n[i] % 2 === 1) {
+        numeroImpar = n[i]
+    }
+}
+
+print(numeroPar)
+print(numeroImpar) */
---
+++ aulas/08 - avaliacao/funcoes-auxiliares-ex2.js
@@ -0,0 +1,13 @@
+/* Minha tentativa:
+
+const numeros = [5, 3, 4, 1, 10, 8]
+
+function gets() {
+    return numeros
+}
+
+function print(texto) {
+    console.log(texto)
+}
+
+module.exports = {gets, print} */
