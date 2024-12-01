a5ed20ad242b3bdc5755cf7c3c4cbcccb3eff90e
Everton Rafael Umbelino dos Santos <169084381+evertonrafaelumbelino@users.noreply.github.com>
2024-12-01 15:03:46 -0300

    atualizei exercicio 3

--- aulas/08 - avaliacao/exercicio3.js
+++ aulas/08 - avaliacao/exercicio3.js
@@ -46,11 +46,27 @@ print(calculoSalario) */
 
 const {gets, print} = require('./funcoes-auxiliares-ex3')
 
-const valorSalarioBruto = gets()
-const valorAdicionalDosBeneficios = gets()
+const valorSalario = gets()
+const valorBeneficios = gets()
 
 function calcularPorcentagem(salario, percentual) {
     return salario * (percentual / 100)
 }
 
-print(calcularPorcentagem(valorSalarioBruto, 10))
+function pegarAliquota(salario) {
+    if (salario >= 0 && salario < 1100) {
+        return 5
+    } else if (salario >= 1100.01 && salario <= 2500) {
+        return 10
+    } else {
+        return 15
+    }
+}
+
+const aliquotaImposto = pegarAliquota(valorSalario)
+
+const valorImposto = (calcularPorcentagem(valorSalario, aliquotaImposto))
+
+const valorATranferir = valorSalario - valorImposto + valorBeneficios
+
+print(valorATranferir)
