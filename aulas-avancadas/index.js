9314dccc2b69a0df5a11a02ae03a45e1b45e46d4
Everton Rafael Umbelino dos Santos <169084381+evertonrafaelumbelino@users.noreply.github.com>
2024-12-25 12:48:34 -0300

    atualizei index.js

--- aulas-avancadas/index.js
+++ aulas-avancadas/index.js
@@ -1,5 +1,8 @@
 const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-lista.forEach((value) => {
-    console.log(value)
+const listaDeNumerosPares = lista.filter((element) => {
+    return(element % 2 === 0)
 })
+
+console.log(lista)
+console.log(listaDeNumerosPares)
