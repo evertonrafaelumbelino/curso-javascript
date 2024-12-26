4f62b0e36fd3a4623ae33a1a59ef1f75e6b6573e
Everton Rafael Umbelino dos Santos <169084381+evertonrafaelumbelino@users.noreply.github.com>
2024-12-26 08:26:00 -0300

    atualizei index.js

--- aulas-avancadas/index.js
+++ aulas-avancadas/index.js
@@ -1,7 +1,8 @@
-const lista = []
+const lista = [{nome: 'Renan'}, {nome: 'Andresa'}, {nome: 'Vitor'}, {nome: 'Amanda'}]
 
-const somaDeTodosOsNumeros = lista.reduce((previous, current) => {
-    return previous + current
-}, 0)
+const elementosEmHtml = lista
+    .filter((e) => e.nome.startsWith('A'))
+    .map((e) => `<li>${e.nome}</li>`)
+    .join('')
 
-console.log(somaDeTodosOsNumeros)
+console.log(elementosEmHtml)
