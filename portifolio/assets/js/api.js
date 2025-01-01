feb6dc3c6aa8858273cf3810a3501c778c35a1ab
Everton Rafael Umbelino dos Santos <169084381+evertonrafaelumbelino@users.noreply.github.com>
2025-01-01 07:16:59 -0300

    atualizei informações do portifolio

--- portifolio/assets/js/api.js
+++ portifolio/assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData () {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/refs/heads/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/evertonrafaelumbelino/curso-javascript/refs/heads/main/portifolio/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
